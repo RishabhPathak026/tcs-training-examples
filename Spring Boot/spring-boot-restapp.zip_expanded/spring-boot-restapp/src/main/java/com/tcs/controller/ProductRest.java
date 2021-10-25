c17b1bfdb3823903f16c4cd51f8c779a3bcb767d
RishabhPathak026 <rishabhpathak2607@gmail.com>
2021-10-25 13:47:42 +0530

    day 5 assignment

--- Spring Boot/spring-boot-restapp.zip_expanded/spring-boot-restapp/src/main/java/com/tcs/controller/ProductRest.java
+++ Spring Boot/spring-boot-restapp.zip_expanded/spring-boot-restapp/src/main/java/com/tcs/controller/ProductRest.java
@@ -1,6 +1,5 @@
 package com.tcs.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.tcs.beans.CustomResponse;
 import com.tcs.beans.Product;
 import com.tcs.exception.ProductNotFoundException;
 import com.tcs.service.ProductService;
@@ -28,42 +27,56 @@ public class ProductRest {
 	private ProductService service;
 	
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Product saveProduct(@RequestBody Product product) {
-		return service.store(product);
+	public ResponseEntity<Object> saveProduct(@RequestBody Product product) {
+		ResponseEntity<Object> response = 
+				ResponseEntity.status(HttpStatus.CREATED).body(service.store(product));
+		return response;
 	}
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Product> getProducts() {
-		return service.fetchProducts();
+	public ResponseEntity<Object> getProducts() {
+		return ResponseEntity.status(HttpStatus.OK).body(service.fetchProducts());
 	}
 	// product/1, product/2, product/3
 	@GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Product> findProduct(@PathVariable("id") int productId) {
+	public ResponseEntity<Object> findProduct(@PathVariable("id") int productId) {
+		ResponseEntity<Object> response = null;
 		try {
-		return new ResponseEntity<Product>(service.fetchProductById(productId),HttpStatus.OK);
-	}catch(ProductNotFoundException e) {
-		return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
-	}
+			Product product = service.fetchProductById(productId);
+			response = ResponseEntity.status(HttpStatus.OK).body(product);
+		} catch(ProductNotFoundException e) {
+			CustomResponse data = new CustomResponse();
+			data.setMsg(e.getMessage());
+			response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(data);
+		}
+		return response;
 	}
 	// product/1/15000
 	@PutMapping(path = "{id}/{price}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Product> updateProductPrice(@PathVariable("id") int id, @PathVariable("price") double price) {
+	public ResponseEntity<Object> updateProductPrice(@PathVariable("id") int id, @PathVariable("price") double price) {
+		ResponseEntity<Object> response = null;
 		try {
-			return new ResponseEntity<Product>(service.updateProductPrice(id, price),HttpStatus.OK);
-		}catch(ProductNotFoundException e) {
-			return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
-		
-	}
+			Product product = service.updateProductPrice(id, price);
+			response = ResponseEntity.status(HttpStatus.OK).body(product);
+		} catch(ProductNotFoundException e) {
+			CustomResponse data = new CustomResponse();
+			data.setMsg(e.getMessage());
+			response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(data);
+		}
+		return response;
 	}
 	@DeleteMapping(path = "{id}")
-	public ResponseEntity<String> deleteProduct(@PathVariable("id") int id) {
-		
-		
+	public ResponseEntity<Object> deleteProduct(@PathVariable("id") int id) {
+		ResponseEntity<Object> response = null;
 		try {
 			service.deleteProductById(id);
-			return new ResponseEntity(HttpStatus.OK);
-		}catch(ProductNotFoundException e) {
-			return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
-		
-	}
+			CustomResponse data = new CustomResponse();
+			data.setMsg("Product with an id "+id+" deleted");
+			response = ResponseEntity.status(HttpStatus.OK).body(data);
+		} catch(ProductNotFoundException e) {
+			CustomResponse data = new CustomResponse();
+			data.setMsg(e.getMessage());
+			response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(data);
+		}
+		return response;
 	}
 }
